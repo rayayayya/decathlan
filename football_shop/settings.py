437b92cafbe6d5db6cd9b0a187b804d107730d49
rayayayya <raynabalq.s@gmail.com>
2025-09-15 21:14:51 +0700

    tambah csrf toker

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -30,7 +30,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rayna-balqis-decathlan.pbp.cs.ui.ac.id"]
-CSRF_TOKEN = []
+CSRF_TOKEN = ["https://pbp.cs.ui.ac.id/rayna.balqis/decathlan"]
 
 # Application definition
 
