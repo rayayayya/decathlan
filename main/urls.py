de5f2daf6b881ca3dac46d2691483f3dbd5bb79b
rayayayya <raynabalq.s@gmail.com>
2025-09-15 21:21:55 +0700

    cek

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from main.views import show_main, create_product, show_products
+from main.views import show_main, create_product, show_products, show_xml
+from main.views import show_main, create_product, show_products, show_xml, show_json
 from main.views import show_main, create_product, show_products, show_xml, show_json, show_xml_by_id, show_json_by_id
-
 app_name = 'main'
 
 urlpatterns = [
